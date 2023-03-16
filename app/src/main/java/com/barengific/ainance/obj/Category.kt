f843cf5c4ffae1a082208ec37b6b5f6e12eebb32
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-16 19:48:32 +0000

    Create Category.kt

---
+++ app/src/main/java/com/barengific/ainance/obj/Category.kt
@@ -0,0 +1,12 @@
+package com.barengific.ainance.obj
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Category(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "name") var name: String?,
+    @ColumnInfo(name = "colour") var colour: String?,
+)
