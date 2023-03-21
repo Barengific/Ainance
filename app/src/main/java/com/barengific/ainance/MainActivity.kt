39ee07498658e3c4cf6d0008728a879084a426bd
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-21 19:37:14 +0000

    Update MainActivity.kt

--- app/src/main/java/com/barengific/ainance/MainActivity.kt
+++ app/src/main/java/com/barengific/ainance/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.barengific.ainance
 
 import android.R
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.barengific.ainance.databinding.ActivityMainBinding
+import com.barengific.ainance.obj.Category
+import com.barengific.ainance.obj.Expense
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
@@ -35,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
+//        hideSystemBars()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -52,6 +61,16 @@ class MainActivity : AppCompatActivity() {
         binding.etPrice.minWidth = (width/2)
         binding.etCategory.minWidth = (width/2)
         binding.btnDate.minWidth = (width/2)
+
+
+        //DB impl
+//        val room = Room.databaseBuilder(applicationContext,
+//            AppDatabase::class.java,
+//            "database-names")
+//            .allowMainThreadQueries()
+//            .build()
+//        val expenseDao = room.expenseDao()
+
 
 
 
@@ -86,6 +105,19 @@ class MainActivity : AppCompatActivity() {
         pieChart = binding.pieCharter
         setupPieChart();
         loadPieChartData();
+
+
+        binding.btnAdd.setOnClickListener {
+            val aa = Expense(
+                0,
+                binding.etName.editText.toString(),
+                Category(0, binding.actCategory.text.toString(), "/"),
+                binding.etPrice.editText.toString(),
+                binding.btnDate.text.toString()
+            )
+//            expenseDao.insertAll(aa)
+
+        }
     }
 
     private fun setupPieChart() {
@@ -133,5 +165,62 @@ class MainActivity : AppCompatActivity() {
         pieChart!!.invalidate()
         pieChart!!.animateY(1400, Easing.EaseInOutQuad)
     }
+
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+}
+
+class RvAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<RvAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView1: TextView
+        val textView2: TextView
+        val textView3: TextView
+        val textView4: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View
+            textView1 = view.findViewById(R.id.textView1)
+            textView2 = view.findViewById(R.id.textView2)
+            textView3 = view.findViewById(R.id.textView3)
+            textView4 = view.findViewById(R.id.textView4)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.rv_row, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView1.text = dataSet[position]
+        viewHolder.textView2.text = dataSet[position]
+        viewHolder.textView3.text = dataSet[position]
+        viewHolder.textView4.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
 }
 
