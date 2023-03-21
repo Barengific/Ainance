ecb6c51e3bd45367e7aed0d74c4b53c3cca5a34c
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-21 19:50:46 +0000

    Update Income.kt

--- app/src/main/java/com/barengific/ainance/obj/Income.kt
+++ app/src/main/java/com/barengific/ainance/obj/Income.kt
@@ -9,7 +9,7 @@ import com.barengific.ainance.obj.Category
 data class Income(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "description") val description: String?,
-    @ColumnInfo(name = "category") var category: Category?,
+    @ColumnInfo(name = "category") var category: String?,
     @ColumnInfo(name = "payed") var payed: String?,
     @ColumnInfo(name = "date") var date: String?
 )
