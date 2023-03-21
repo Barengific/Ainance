f772f26c1673043bbc5153da4f9a3e36a6034306
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-21 19:38:03 +0000

    Update MainActivity.kt

--- app/src/main/java/com/barengific/ainance/MainActivity.kt
+++ app/src/main/java/com/barengific/ainance/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-//        hideSystemBars()
+        hideSystemBars()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -177,50 +177,50 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class RvAdapter(private val dataSet: Array<String>) :
-    RecyclerView.Adapter<RvAdapter.ViewHolder>() {
-
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView1: TextView
-        val textView2: TextView
-        val textView3: TextView
-        val textView4: TextView
-
-        init {
-            // Define click listener for the ViewHolder's View
-            textView1 = view.findViewById(R.id.textView1)
-            textView2 = view.findViewById(R.id.textView2)
-            textView3 = view.findViewById(R.id.textView3)
-            textView4 = view.findViewById(R.id.textView4)
-        }
-    }
-
-    // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.rv_row, viewGroup, false)
-
-        return ViewHolder(view)
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.textView1.text = dataSet[position]
-        viewHolder.textView2.text = dataSet[position]
-        viewHolder.textView3.text = dataSet[position]
-        viewHolder.textView4.text = dataSet[position]
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = dataSet.size
-
-}
+//class RvAdapter(private val dataSet: Array<String>) :
+//    RecyclerView.Adapter<RvAdapter.ViewHolder>() {
+//
+//    /**
+//     * Provide a reference to the type of views that you are using
+//     * (custom ViewHolder)
+//     */
+//    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val textView1: TextView
+//        val textView2: TextView
+//        val textView3: TextView
+//        val textView4: TextView
+//
+//        init {
+//            // Define click listener for the ViewHolder's View
+//            textView1 = view.findViewById(R.id.textView1)
+//            textView2 = view.findViewById(R.id.textView2)
+//            textView3 = view.findViewById(R.id.textView3)
+//            textView4 = view.findViewById(R.id.textView4)
+//        }
+//    }
+//
+//    // Create new views (invoked by the layout manager)
+//    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+//        // Create a new view, which defines the UI of the list item
+//        val view = LayoutInflater.from(viewGroup.context)
+//            .inflate(R.layout.rv_row, viewGroup, false)
+//
+//        return ViewHolder(view)
+//    }
+//
+//    // Replace the contents of a view (invoked by the layout manager)
+//    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+//
+//        // Get element from your dataset at this position and replace the
+//        // contents of the view with that element
+//        viewHolder.textView1.text = dataSet[position]
+//        viewHolder.textView2.text = dataSet[position]
+//        viewHolder.textView3.text = dataSet[position]
+//        viewHolder.textView4.text = dataSet[position]
+//    }
+//
+//    // Return the size of your dataset (invoked by the layout manager)
+//    override fun getItemCount() = dataSet.size
+//
+//}
 
