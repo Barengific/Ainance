049f0516814182adf909939ebe4dce17cd3335fe
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-23 19:25:34 +0000

    Update AppDatabase.kt

--- app/src/main/java/com/barengific/ainance/AppDatabase.kt
+++ app/src/main/java/com/barengific/ainance/AppDatabase.kt
@@ -6,12 +6,10 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.barengific.ainance.dao.CategoryDao
 import com.barengific.ainance.dao.ExpenseDao
-import com.barengific.ainance.dao.IncomeDao
 import com.barengific.ainance.obj.Category
 import com.barengific.ainance.obj.Expense
-import com.barengific.ainance.obj.Income
 
-@Database(entities = [Expense::class, Income::class, Category::class], version = 1)
+@Database(entities = [Expense::class, Category::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         private lateinit var INSTANCE:AppDatabase
@@ -29,7 +27,6 @@ abstract class AppDatabase : RoomDatabase() {
     }
 
     abstract fun expenseDao(): ExpenseDao
-    abstract fun incomeDao(): IncomeDao
     abstract fun categoryDao(): CategoryDao
 
 }
