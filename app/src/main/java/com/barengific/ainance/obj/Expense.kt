a05b5a922fc5d834ad96345b69059cb5dc83182c
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-26 21:10:53 +0000

    Update Expense.kt

--- app/src/main/java/com/barengific/ainance/obj/Expense.kt
+++ app/src/main/java/com/barengific/ainance/obj/Expense.kt
@@ -3,11 +3,13 @@ package com.barengific.ainance.obj
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
+
 @Entity
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "description") val description: String?,
     @ColumnInfo(name = "withdraw") var withdraw: String?,
     @ColumnInfo(name = "category") var category: String?,
-    @ColumnInfo(name = "date") var date: String?
+    @ColumnInfo(name = "date") var date: Date?
 )
