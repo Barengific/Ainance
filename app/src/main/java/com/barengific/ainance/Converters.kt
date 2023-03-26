0aa21062f1d4f270e1189c80baaedd7e2fe699e5
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-26 21:10:42 +0000

    Create Converters.kt

---
+++ app/src/main/java/com/barengific/ainance/Converters.kt
@@ -0,0 +1,17 @@
+package com.barengific.ainance
+
+import androidx.room.TypeConverter
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Converters {
+    @TypeConverter
+    fun fromDate(date: Date?): String? {
+        return date?.let { SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(it) }
+    }
+
+    @TypeConverter
+    fun toDate(dateString: String?): Date? {
+        return dateString?.let { SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).parse(it) }
+    }
+}
