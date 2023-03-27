de61f38d0fc55318b8d571bf294c9255553c4497
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-27 17:21:49 +0000

    Update Expense.kt

--- app/src/main/java/com/barengific/ainance/obj/Expense.kt
+++ app/src/main/java/com/barengific/ainance/obj/Expense.kt
@@ -3,9 +3,12 @@ package com.barengific.ainance.obj
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.barengific.ainance.Converters
 import java.util.*
 
 @Entity
+@TypeConverters(Converters::class)
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "description") val description: String?,
