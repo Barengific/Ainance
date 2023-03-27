fd179afb506d9c226e1b1f9d2253fef95a0b1fc5
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-27 19:59:29 +0000

    Update Expense.kt

--- app/src/main/java/com/barengific/ainance/obj/Expense.kt
+++ app/src/main/java/com/barengific/ainance/obj/Expense.kt
@@ -8,11 +8,10 @@ import com.barengific.ainance.Converters
 import java.util.*
 
 @Entity
-@TypeConverters(Converters::class)
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "description") val description: String?,
     @ColumnInfo(name = "withdraw") var withdraw: String?,
     @ColumnInfo(name = "category") var category: String?,
-    @ColumnInfo(name = "date") var date: Date?
+    @ColumnInfo(name = "date") var date: String?
 )
